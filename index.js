2762aaac42baef4141d3857c24b16d4aec896555
Troy Lawrence <redirect@gmail.com>
2018-02-06 20:29:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+let musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr" ];
+let instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+
+function theBeatlesPlay (musicians, instruments) {
+  for (let i=0; i<3; i++) {
+    let phrase = [];
+    phrase.push(`"${musicians.i} plays  ${instruments.i}."`);
+  }
+};
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts (facts) {
+  while (facts.length == 4) {
+    let phrase = [];
+    phrase.push(`"${facts[0]}!!!"`)
+  }
+}
+
+function iLoveTheBeatles (number) {
+  let phrase = [];
+  do {phrase.push("I love the Beatles!") }
+    while (number<15)
+}
